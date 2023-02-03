bf2b593670c3651b76a71871a9d58d40a2cb9a6d
Alexander Desyatovskiy <alekc.saha@mail.ru>
2023-02-03 18:34:04 +0300

    upd

--- main.py
+++ main.py
@@ -6,24 +6,23 @@ if __name__ == '__main__':
     running = True
     pygame.init()
     coords = input().replace(' ', ',')
-    scale = input()
+    scale = int(input())
     map_api_server = "http://static-maps.yandex.ru/1.x/"
-    map_params = {
-        "ll": coords,
-        "spn": ",".join([scale, scale]),
-        "l": "map"
-    }
-    response = requests.get(map_api_server, params=map_params)
     map_file = 'map.png'
-    with open(map_file, 'wb') as file:
-        file.write(response.content)
-
     screen = pygame.display.set_mode((600, 450))
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         screen.fill((255, 255, 255))
+        map_params = {
+            "ll": coords,
+            "spn": ",".join([str(scale), str(scale)]),
+            "l": "map"
+        }
+        response = requests.get(map_api_server, params=map_params)
+        with open(map_file, 'wb') as file:
+            file.write(response.content)
         screen.blit(pygame.image.load(map_file), (0, 0))
         pygame.display.flip()
     pygame.quit()
