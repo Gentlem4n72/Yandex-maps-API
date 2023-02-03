99af4973a9e38dd373d50089c91bad37eab1a1a1
Alexander Desyatovskiy <alekc.saha@mail.ru>
2023-02-03 19:19:25 +0300

    added changing map type

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def get_map_parameters(json_response):
 
     map_params = {
         "ll": ",".join([toponym_longitude, toponym_lattitude]),
-        "l": "map",
+        "l": ['map', 'sat', 'sat,skl'][map_type],
         'z': scale
     }
     return map_params
@@ -38,15 +38,22 @@ def map(toponym_to_find):
     return response
 
 
+def draw_map_type_selection(screen):
+    pygame.draw.rect(screen, 'black', (5, 455, 200, 40), 3)
+    text = pygame.font.Font(None, 30).render(['Схема', 'Спутник', 'Гибрид'][map_type], True, 'black')
+    screen.blit(text, (103 - text.get_width() // 2, 478 - text.get_height() // 2))
+
+
 if __name__ == '__main__':
     running = True
     pygame.init()
     # coords = input().replace(' ', ',')
     scale = 17
+    map_type = 0
     toponym_to_find = 'Санкт-Петербург, набережная Мойки, 14'
 
     map_file = 'map.png'
-    screen = pygame.display.set_mode((600, 450))
+    screen = pygame.display.set_mode((600, 500))
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -54,10 +61,13 @@ if __name__ == '__main__':
             if event.type == pygame.KEYDOWN and event.key == pygame.K_PAGEUP:
                 if scale < 23:
                     scale += 1
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_PAGEDOWN:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_PAGEDOWN:
                 if scale > 0:
                     scale -= 1
+            if event.type == pygame.MOUSEBUTTONDOWN and 5 <= event.pos[0] <= 205 and 455 <= event.pos[1] <= 495:
+                map_type = (map_type + 1) % 3
         screen.fill((255, 255, 255))
+        draw_map_type_selection(screen)
         response = map(toponym_to_find)
         with open(map_file, 'wb') as file:
             file.write(response.content)
