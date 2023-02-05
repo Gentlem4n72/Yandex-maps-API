fbe895c421aeff6fe6c666a646afe007ce447dda
ScaryOrange <UnKindledastor@yandex.ru>
2023-02-05 16:23:52 +0300

    search str

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ def get_map_parameters(json_response):
         "featureMember"][0]["GeoObject"]
     toponym_coodrinates = toponym["Point"]["pos"]
     toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+    ll = ",".join([toponym_longitude, toponym_lattitude])
 
     map_params = {
-        "ll": ",".join([toponym_longitude, toponym_lattitude]),
+        "ll": ll,
         "l": ['map', 'sat', 'sat,skl'][map_type],
+        'pt': f'{ll},pm2ywm',
         'z': scale
     }
     return map_params
@@ -48,12 +50,17 @@ if __name__ == '__main__':
     running = True
     pygame.init()
     # coords = input().replace(' ', ',')
+    font = pygame.font.Font(None, 35)
+    font1 = pygame.font.Font(None, 25)
+    flag = False
     scale = 17
     map_type = 0
     toponym_to_find = 'Санкт-Петербург, набережная Мойки, 14'
-
+    input_field = pygame.Rect(220, 455, 300, 40)
+    search_button = pygame.Rect(522, 455, 70, 40)
     map_file = 'map.png'
     screen = pygame.display.set_mode((600, 500))
+    text = ''
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -66,8 +73,25 @@ if __name__ == '__main__':
                     scale -= 1
             if event.type == pygame.MOUSEBUTTONDOWN and 5 <= event.pos[0] <= 205 and 455 <= event.pos[1] <= 495:
                 map_type = (map_type + 1) % 3
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if input_field.collidepoint(event.pos):
+                    flag = True
+                else:
+                    flag = False
+                if search_button.collidepoint(event.pos):
+                    if text != '':
+                        toponym_to_find = text
+            if event.type == pygame.KEYDOWN and flag:
+                if event.key == pygame.K_BACKSPACE:
+                    text = text[:-1]
+                else:
+                    text += event.unicode
         screen.fill((255, 255, 255))
         draw_map_type_selection(screen)
+        screen.blit(font.render(text, True, 'black'), (input_field.x + 5, input_field.y + 5))
+        screen.blit(font1.render('search', True, 'red'), (search_button.x + 8, search_button.y + 10))
+        pygame.draw.rect(screen, 'black', input_field, 1)
+        pygame.draw.rect(screen, 'black', search_button, 1)
         response = map(toponym_to_find)
         with open(map_file, 'wb') as file:
             file.write(response.content)
